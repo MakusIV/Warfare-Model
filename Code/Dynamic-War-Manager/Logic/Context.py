4cfbb3d06ff5457b16bf4899760f82a0d1c3deef
marco <marco.bellafante@gmail.com>
2024-12-11 18:00:01 +0100

    update UML: new class Area, Volume, Threat, Region, Unit_Area; update CODE: \Logic new class Area, modifed Context module, create python  virtual enviroment and installed SymPy

--- Code/Dynamic-War-Manager/Logic/Context.py
+++ Code/Dynamic-War-Manager/Logic/Context.py
@@ -4,6 +4,28 @@
  Elenco delle variabili di contesto
 
 """
+from enum import Enum
+
+class STATE(Enum):
+    Active = 1
+    Inactive = 2
+    Standby = 3
+    Destroyed = 4
+    
+class SHAPE3D(Enum):
+    Cylinder = 1
+    Cube = 2
+    Sphere = 3
+    SemiSphere = 4
+    Pyramid = 5
+    Cone = 6
+    Trunc_Cone = 7
+
+class SHAPE2D(Enum):
+    Circonference = 1
+    Square = 2
+    Hexagon = 3
+    
 
 
-STATE = ( "Active", "Inactive", "Standby", "Detroyed" ) # non serve inserire Dameged in quanto è un valore che determina se l'asset è attivo, non attivo o distrutto 
+
