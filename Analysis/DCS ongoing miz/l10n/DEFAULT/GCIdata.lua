c03aafb00da52044795ed86fa0cdfa246b7ac93c
marco <marco.bellafante@gmail.com>
2025-01-09 15:55:36 +0100

    added DCS ongoing_mission.miz in Analisys

---
+++ Analysis/DCS ongoing miz/l10n/DEFAULT/GCIdata.lua
@@ -0,0 +1,113 @@
+GCI = {
+	['Flag'] = 504,
+	['Interceptor'] = {
+		['blue'] = {
+			['base'] = {
+				['CVN-71 Theodore Roosevelt'] = {
+					['ready15'] = {
+					},
+					['ready30'] = {
+					},
+					['ready_n'] = 1,
+					['ready15_n'] = 0,
+					['ready'] = {
+						[1] = {
+							['number'] = 1,
+							['airdromeId'] = 831,
+							['flag'] = 501,
+							['range'] = 250000,
+							['y'] = 451537,
+							['x'] = -235099,
+							['name'] = 'Pack 1 - VF-101 - Intercept 1',
+							['time'] = -900,
+							['tot_from'] = 0,
+							['tot_to'] = 999999,
+						},
+					},
+				},
+			},
+			['assigned'] = {
+			},
+		},
+		['red'] = {
+			['base'] = {
+				['Beslan'] = {
+					['ready15'] = {
+					},
+					['ready30'] = {
+					},
+					['ready_n'] = 1,
+					['ready15_n'] = 0,
+					['ready'] = {
+						[1] = {
+							['number'] = 1,
+							['airdromeId'] = 32,
+							['flag'] = 503,
+							['range'] = 120000,
+							['y'] = 843756.7533062,
+							['x'] = -148810.84954665,
+							['name'] = 'Pack 4 - 1./37.IAP - Intercept 1',
+							['time'] = -900,
+							['tot_from'] = 0,
+							['tot_to'] = 32067,
+						},
+					},
+				},
+				['Mozdok'] = {
+					['ready15'] = {
+						[1] = {
+							['number'] = 2,
+							['airdromeId'] = 28,
+							['flag'] = 504,
+							['range'] = 200000,
+							['y'] = 834453.14285714,
+							['x'] = -83454.571428571,
+							['name'] = 'Pack 6 - 1./113.IAP - Intercept 1',
+							['time'] = -900,
+							['tot_from'] = 0,
+							['tot_to'] = 999999,
+						},
+					},
+					['ready30'] = {
+					},
+					['ready_n'] = 1,
+					['ready15_n'] = 1,
+					['ready'] = {
+						[1] = {
+							['number'] = 1,
+							['airdromeId'] = 28,
+							['flag'] = 502,
+							['range'] = 120000,
+							['y'] = 834453.14285714,
+							['x'] = -83454.571428571,
+							['name'] = 'Pack 3 - 790.IAP - Intercept 1',
+							['time'] = -900,
+							['tot_from'] = 0,
+							['tot_to'] = 999999,
+						},
+					},
+				},
+			},
+			['assigned'] = {
+			},
+		},
+	},
+	['EWR'] = {
+		['blue'] = {
+			['Unit #281'] = true,
+			['LHA_Tarawa'] = true,
+			['CVN-74 John C. Stennis'] = true,
+			['CG-69 Viksburg'] = true,
+			['Pack 37 - 7 ACCS - AWACS 1-1'] = true,
+			['Unit #274'] = true,
+			['Unit #267'] = true,
+			['CG-60 Ticonderoga'] = true,
+			['CG-68 Anzio'] = true,
+		},
+		['red'] = {
+			['EWR Station West'] = true,
+			['Pack 15 - 2457.I SDRLO - AWACS 2-1'] = true,
+			['Pack 15 - 2457.I SDRLO - AWACS 1-1'] = true,
+		},
+	},
+}
